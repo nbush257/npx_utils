9719e3e921627df6c9436e27dad654e1e1e678a9
Bush, Nicholas <Nicholas.Bush@seattlechildrens.org>
2020-08-21 14:15:02 -0700

    added a function to use the extracted timestamps from catGT to synch the NI and IMEC probes. May be unneeded

--- data.py
+++ data.py
@@ -1,1 +1,41 @@
 """Routines for data import and manipulation."""
+import sys
+import numpy as np
+import readSGLX
+sys.path.append('../')
+from utils.ephys.signal import binary_onsets
+
+def get_tvec(x_sync,sync_timestamps,sr):
+    '''
+    Get a time vector for NI that uses the sync signal
+
+    :param x_sync:
+    :param sync_timestamps:
+    :return: tvec
+    '''
+    tvec = np.empty_like(x_sync)
+    onsets,offsets = binary_onsets(x_sync,1)
+    n_ons = len(onsets)
+    n_ts = len(sync_timestamps)
+    if n_ons!=n_ts:
+        raise ValueError(f"number of detected onsets {n_ons} does not match number expected {n_ts}")
+
+    # Map all detected onsets to a timestamp and linearly interpolate
+    for ii in range(len(onsets)-1):
+        nsamps = onsets[ii+1]-onsets[ii]
+        temp = np.linspace(sync_timestamps[ii],sync_timestamps[ii+1],nsamps)
+        tvec[onsets[ii]:onsets[ii+1]] = temp
+
+    # get times before first synch signal
+    first_t = sync_timestamps[0]-onsets[0]/sr
+    first_seg = np.linspace(first_t,sync_timestamps[0],onsets[0])
+    tvec[:onsets[0]] = first_seg
+
+    # get times after last synch signal
+    last_seg_length = len(x_sync)-onsets[-1]
+    last_seg = np.linspace(sync_timestamps[-1],sync_timestamps[-1] + last_seg_length/sr,last_seg_length)
+    tvec[onsets[-1]:] = last_seg
+
+    return(tvec)
+
+
