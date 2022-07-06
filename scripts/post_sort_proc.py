d5741c682900c2c2134095768678507644bcef59
nbush <nbush257@gmail.com>
2022-07-06 09:34:26 -0700

    removed unused code for CCG,CMA, compute_gasp_mod,get_Connectivity,post_sort_proc

--- scripts/post_sort_proc.py
+++
@@ -1,9 +0,0 @@
-'''
-Given a directory of the Kilosort results, this function uses the IBL libraries to compute metrics on the sorted neurons.
-Potentially - can be rerun after manual curation
-Potentially- pipes into Tprime and Cwaves
-'''
-
-import ibllib
-import sys
-import os
