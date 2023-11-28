8980d3b07485f7a9b37d313b302f4438c8e656b1
nbush <nbush257@gmail.com>
2023-11-28 11:34:11 -0800

    can convert from Sharptrtack or AP_histology output to bregma related coords

--- scripts/convert_ap_histology_to_IBL.py
+++ scripts/convert_ap_histology_to_IBL.py
@@ -15,16 +15,14 @@ import click
 res = 10
 brain_atlas = atlas.AllenAtlas(res)
 
-
-@click.command()
-@click.argument('fn')
-def main(fn):
+def convert_from_AP_histology(fn):
     '''
     fn - File name of the probeCCF file output from AP_histology
     Writes xyz_json files to the same folder. You will have to  manually move them to the appropriate alf folder.
     
     '''
     # Transform coords from CCF origin (order apdvml) to Bregma origin (order mlapdv)
+    print('Converting from AP_histology')
     fn = Path(fn)
     dat_in = sio.loadmat(fn)
     n_probes = dat_in['probe_ccf'].shape[0]
@@ -36,6 +34,39 @@ def main(fn):
         output_path = fn.parent
         with open(Path(output_path, f'xyz_picks{probe_ID}.json'), "w") as f:
             json.dump(xyz_picks, f, indent=2)
+
+def convert_from_SHARPTRACK(fn):
+    '''
+    Given a probe_points.mat file find all the probe matrices and conver to IBL. WIll write an xyz_json in the same folder.
+    You will need to manually move to the appropriate ALF folder
+    '''
+    print('Converting from sharptrack')
+    fn = Path(fn)
+    dat_in = sio.loadmat(fn)
+    n_probes = dat_in['pointList'][0,0][0].shape[0]
+
+    for probe_ID in range(n_probes):
+        points = dat_in['pointList'][0,0][0][probe_ID,0]
+        ccf_apdvml = points[:,[2,1,0]]*10
+        # May need to sort
+        xyz_mlapdv = brain_atlas.ccf2xyz(ccf_apdvml, ccf_order='apdvml') * 1e6 # convert bregma relative in meters to bregma relative in uM
+        xyz_picks = {'xyz_picks': xyz_mlapdv.tolist()}
+
+        output_path = fn.parent
+        with open(Path(output_path, f'xyz_picks{probe_ID+1}.json'), "w") as f:
+            json.dump(xyz_picks, f, indent=2)
+
+
+@click.command()
+@click.argument('fn')
+@click.argument('mode')
+def main(fn,mode):
+    if mode=='AP':
+        convert_from_AP_histology(fn)
+    elif mode=='ST':
+        convert_from_SHARPTRACK(fn)
+    else:
+        raise NotImplementedError('Mode is not implemented. Must be "AP" or "ST"')
 
 if __name__=='__main__':
     main()
